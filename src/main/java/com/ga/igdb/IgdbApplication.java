b71cd4f196e1adcd2c63b48506340e5038b75399
Ghaidaa Hussain <gadhus@outlook.com>
2021-01-26 14:17:56 +0300

    started project

---
+++ src/main/java/com/ga/igdb/IgdbApplication.java
@@ -0,0 +1,13 @@
+package com.ga.igdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgdbApplication.class, args);
+	}
+
+}
