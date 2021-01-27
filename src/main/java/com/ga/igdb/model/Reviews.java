aa89cab63ab8631127761e19f471f5ef9f62377b
amalalanazi <ammal_77@hotmail.com>
2021-01-27 15:09:42 +0300

    Created Reviews.java in model package

--- src/main/java/com/ga/igdb/model/Reviews.java
+++ src/main/java/com/ga/igdb/model/Reviews.java
@@ -1,5 +1,75 @@
 package com.ga.igdb.model;
 
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name = "Reviews")
 public class Reviews {
+	@Id
+	@GeneratedValue
+	private int id;
+	private double rate;
+	private String reviewDes;
+
+//	@ManyToOne
+//	@JoinColumn(name = "FK_GameId")
+//	
+//	@ManyToOne
+//	@JoinColumn(name = "FK_UserId")
+
+	@Column(name = "createdAt", nullable = false, updatable = false)
+	@CreationTimestamp
+	private LocalDateTime createAt;
+
+	@Column(name = "updatedat", nullable = false, updatable = true)
+	@UpdateTimestamp
+	private LocalDateTime updateAt;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public double getRate() {
+		return rate;
+	}
+
+	public void setRate(double rate) {
+		this.rate = rate;
+	}
+
+	public String getReviewDes() {
+		return reviewDes;
+	}
+
+	public void setReviewDes(String reviewDes) {
+		this.reviewDes = reviewDes;
+	}
+
+	public LocalDateTime getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(LocalDateTime createAt) {
+		this.createAt = createAt;
+	}
+
+	public LocalDateTime getUpdateAt() {
+		return updateAt;
+	}
+
+	public void setUpdateAt(LocalDateTime updateAt) {
+		this.updateAt = updateAt;
+	}
 
 }
