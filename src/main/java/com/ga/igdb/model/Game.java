25a9fc409cc134a6760067bb17ef7a2a65880a4b
Haya <hm.almadhi@gmail.com>
2021-01-27 16:45:43 +0300

    add games table and properties and set dev profile, NOTE I comment relation and rank because it causes a problem

--- src/main/java/com/ga/igdb/model/Game.java
+++ src/main/java/com/ga/igdb/model/Game.java
@@ -1,5 +1,121 @@
 package com.ga.igdb.model;
+import java.sql.Date;
+import java.time.LocalDateTime;
+import java.util.Set;
+import javax.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+@Entity
+@Table(name="Game")
 public class Game {
+	@Id
+	@GeneratedValue
+	private int id;
+	private String gameName;
+	// cover is a photo of the game
+	private String cover;
+//    private Long rank;
+	private String companyName;
+	private String console;
+	private Date releaseDate;
+	private String category;
+	private String description;
+	// Review video of game play
+	private String demo;
+	
+//	// Relations
+//	//user 1 - N game
+//	//game 1 - N review
+//	
+////	@ManyToOne
+////	@JoinColumn(name = "FK_UserId")
+////	private User user;
+////	
+////	@OneToMany
+////	@JoinColumn(mappedBy = "game")
+////	private Set<Review> reviews;
+	
+	@Column(name="createdAt", nullable = false, updatable = false)
+	@CreationTimestamp
+	private LocalDateTime createAt;
+	
+	public String getGameName() {
+		return gameName;
+	}
+	public void setGameName(String gameName) {
+		this.gameName = gameName;
+	}
+	
+	public LocalDateTime getCreateAt() {
+		return createAt;
+	}
+	public void setCreateAt(LocalDateTime createAt) {
+		this.createAt = createAt;
+	}
+	public LocalDateTime getUpdateAt() {
+		return updateAt;
+	}
+	public void setUpdateAt(LocalDateTime updateAt) {
+		this.updateAt = updateAt;
+	}
+	@Column(name="updatedat", nullable = false, updatable = true)
+	@UpdateTimestamp
+	private LocalDateTime updateAt;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCover() {
+		return cover;
+	}
+	public void setCover(String cover) {
+		this.cover = cover;
+	}
+//	public long getRank() {
+//		return rank;
+//	}
+//	public void setRank(long rank) {
+//		this.rank = rank;
+//	}
+	public String getCompanyName() {
+		return companyName;
+	}
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	public String getConsole() {
+		return console;
+	}
+	public void setConsole(String console) {
+		this.console = console;
+	}
 
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getDemo() {
+		return demo;
+	}
+	public void setDemo(String demo) {
+		this.demo = demo;
+	}
+	public Date getReleaseDate() {
+		return releaseDate;
+	}
+	public void setReleaseDate(Date releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+	
 }
